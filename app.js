c1b31c104b33384bdd90db34d0844d2885228640
T4CEE <cyriliniekong@gmail.com>
2024-09-20 16:26:24 +0100

    first push

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+
+const app = express();
+const port = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//static Files
+app.use(express.static("public"));
+
+//Templating Engine
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+//Routes
+app.use("/", require("./server/routes/index"));
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
